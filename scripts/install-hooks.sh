4bd0d0a4c958c52c41dbfbbae88c4c15ee420d95
Santino2005 <santinocolombo13@gmail.com>
2025-12-01 10:01:02 -0300

    Fix script

--- scripts/install-hooks.sh
+++ scripts/install-hooks.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 # ./scripts/install-hooks.sh
 
+echo "Instalando hooks..."
+
 mkdir -p .git/hooks
 
 cp .github/hooks/pre-commit .git/hooks/pre-commit
@@ -9,4 +11,4 @@ cp .github/hooks/post-commit .git/hooks/post-commit
 chmod +x .git/hooks/pre-commit
 chmod +x .git/hooks/post-commit
 
-echo "Hooks instalados"
+echo "Hooks instalados correctamente."
